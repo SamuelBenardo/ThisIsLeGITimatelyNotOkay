dc7bb996fa320b94eb363ad1dabc90586b6a3de7
charbim <cphelan1@hwemail.com>
2025-09-24 20:45:46 -0700

    (GP-2.2): Created a Hash Function

---
+++ Github.java
@@ -0,0 +1,117 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+public class Github {
+    public static void main(String[] args) throws IOException {
+        String str = hashFile(readFileContent(new File("test.txt")));
+        System.out.println(str);
+        String sha1 = "a9993e364706816aba3e25717850c26c9cd0d89d";
+        System.out.println(sha1.equals(str));
+    }
+
+    // Creates the directories required for the github
+    public static void initializeDirs() throws IOException {
+
+        // Initializes File names + make git dir
+        File[] files = createDirList();
+        boolean isCreated = false;
+
+        if (!files[0].exists()) {
+            files[0].mkdir();
+            isCreated = true;
+            if (!files[1].exists()) {
+                files[1].mkdir();
+            } else {
+                isCreated = false;
+            }
+        }
+
+        // Cycles through list and turns strings into files (if they do not already
+        // exist).
+        for (int i = 2; i < files.length; i++) {
+            if (!files[i].exists()) {
+                files[i].createNewFile();
+            } else {
+                isCreated = false;
+            }
+        }
+
+        // Prints confirmation method
+        if (isCreated) {
+            System.out.println("Git Repository Created");
+        } else {
+            System.out.println("Git Repository Already Exists");
+        }
+
+    }
+
+    // initializes a list of all files to be created
+    public static File[] createDirList() {
+        String dir = "./git";
+        String[] subFiles = new String[] { "", "/objects", "/index", "/HEAD" };
+        File[] files = new File[4];
+
+        for (int i = 0; i < 4; i++) {
+            subFiles[i] = dir + subFiles[i];
+            files[i] = new File(subFiles[i]);
+        }
+
+        return files;
+    }
+
+    public static String readFile(File file) {
+        return "";
+
+    }
+
+    // reads the file contents and returns as a string
+    public static String readFileContent(File f) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            while (br.ready()) {
+                sb.append(br.readLine());
+            }
+            return sb.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    // hashes the given contents into sha-1
+    private static String hashFile(String contents) {
+        String sha1 = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(contents.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return sha1;
+    }
+
+    // converts to bytes
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+}
